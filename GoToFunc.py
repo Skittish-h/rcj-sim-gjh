561ecefb732cdf4fb5d0719081b0156cc300f472
nb159 <heshamalhuraibi1@gmail.com>
2021-02-03 20:09:46 +0100

    improved Func

--- GoToFunc.py
+++ GoToFunc.py
@@ -1,14 +1,42 @@
 import math
 import utils
 import time
+#oprional argument
+#pointToGoal = true it will point to goal else it does nothing 
+#
+"""git add gotofunc.py
+git commit -m "msg for users"
+git push"""
 
-def goTo(Dest_pos, robot_pos, robot_angle):
-    YAxisToDest = Dest_pos[0][1]-robot_pos["y"]
-    XAxisToDest = Dest_pos[0][0]-robot_pos["x"]
+def goTo(x, y, robot_pos, robot_angle):
+    AngleNDistance = GetAngleToSpot(x,y,robot_pos, robot_angle) #0 angle to spot, 1 distance to spot
+    MotorsSpeed = RotateToSpot(AngleNDistance[0]) #0 right, 1 left 
+     
+    RotateToGoal=False
+   
+
+    if(AngleNDistance[1] <=0.05):
+        if(robot_pos["y"] <0.5):
+            MotorsSpeed[0] =-10
+            MotorsSpeed[1] =10
+        else:
+            MotorsSpeed[0] =10
+            MotorsSpeed[1] =-10
+
+    print( MotorsSpeed[0], " ",  MotorsSpeed[1])
+    
+    return [MotorsSpeed[0], MotorsSpeed[1]]
+
+##################################################
+##################################################
+def GetAngleToSpot(x,y,robot_pos, robot_angle):
+    YAxisToDest = y-robot_pos["y"]
+    XAxisToDest = x-robot_pos["x"]
     angle = math.atan2(YAxisToDest, XAxisToDest)
-
+    #gets the hypo to know the distance between robot and desired location
     distanceToSpot = math.sqrt(math.pow(YAxisToDest, 2) + math.pow(XAxisToDest, 2))                   
-    #shiftAngle =angle-robot_angle
+
+    #some math (copied xD) to convert the value we got into degrees so that our robot can get the direction to point at  
     if angle < 0:
         angle = 2 * math.pi + angle
 
@@ -20,23 +48,38 @@ def goTo(Dest_pos, robot_pos, robot_angle):
     if robotDestAngle > 360:
         robotDestAngle -= 360
     
-    #create your own get_direction so that it turns only once from beginning
-    direction = utils.get_direction(robotDestAngle)
-    #print(direction)
-    RotateToGoal=False
-    ForwardSpeed =10
-    RotationSpeed =9
-    if(distanceToSpot <=0.15):
-        ForwardSpeed /=2 
+    return [robotDestAngle, distanceToSpot]
 
-    if(distanceToSpot <=0.09):
-        ForwardSpeed =0;
-        RotateToGoal=True
-        t2= time.time()
-       
-
-    if(RotateToGoal):
-        RotationSpeed =0
-        #RotateToGoal() 
+#####################################################
+#####################################################
+def RotateToSpot (robotAngleFromSpot):
+    #defualt values
+    right = -10
+    left = -10
+    MagicNum =0.67
     
-    return [direction, ForwardSpeed, RotationSpeed, distanceToSpot]
+    angleToTarget = 360-robotAngleFromSpot #actual rotation needed to face desires spot
+    if(angleToTarget > 360): # for some reason the fieled is 360...its 400 somethin i think
+        angleToTarget-=360
+    print(angleToTarget)
+
+    if(angleToTarget >=5 and angleToTarget <180):#turn left
+        left += (angleToTarget *MagicNum)
+        right  +=(-angleToTarget)*MagicNum
+    if(angleToTarget <= 355 and angleToTarget>=180):#turn right  
+        angleToTarget = 360 - angleToTarget
+        left += (-angleToTarget)*MagicNum 
+        right +=  (angleToTarget*MagicNum)*2
+
+    #if motors speed exceed the limit. cap em
+    if(right >10):
+        right= 10
+    elif(right <-10):
+        right= -10
+
+    if(left >10):
+        left= 10
+    elif(left <-10):
+        left= -10
+
+    return [right, left]
