8b067d1d541f904c26809765d186e9e64cf0e233
Gekbako <jakub.gregus@gmail.com>
2021-02-06 20:53:05 +0100

    updated to the role choosing func

--- SupportOptiPos.py
+++ SupportOptiPos.py
@@ -1,27 +1,29 @@
 from CoordinateRecalculator import robot_pos_recalc
 def support_position (data):
-    """ need to know which robot is attack and which one is goalee 
-    x_cor = {"b1":data["B1"]["x"],"b2":data["B2"]["x"],"b3":data["B3"]["x"]}
-    y_cor = {"b1": data["B1"]["y"], "b2": data["B2"]["y"], "b3": data["B3"]["y"]}
+    roles = role_decision(self)
+    b1 = {"x":data["B1"]["x"],"y": data["B1"]["y"]}
+    b2 = {"x":data["B2"]["x"],"y": data["B2"]["y"]}
+    b3 = {"x":data["B3"]["x"],"y": data["B3"]["y"]}
 
-    # checking for attacker and goalie
-    if x_cor["b1"]==max(x_cor.values()):
-        goalie_cor = {"x":x_cor["b1"],"y":y_cor["b1"]}
-    if x_cor["b2"]==max(x_cor.values()):
-        goalie_cor = {"x":x_cor["b2"],"y":y_cor["b2"]}
-    if x_cor["b3"]==max(x_cor.values()):
-        goalie_cor = {"x":x_cor["b3"],"y":y_cor["b3"]}
-    if x_cor["b1"]==min(x_cor.values()):
-        att_cor = {"x":x_cor["b1"],"y":y_cor["b1"]}
-    if x_cor["b2"]==min(x_cor.values()):
-        att_cor = {"x":x_cor["b2"],"y":y_cor["b2"]}
-    if x_cor["b3"]==min(x_cor.values()):
-        att_cor = {"x":x_cor["b3"],"y":y_cor["b3"]}"""
-    
-
-    #calculation of support optimal position
-    b2 = robot_pos_recalc(data["B2"])
-    b3 = robot_pos_recalc(data["B3"])
-    supp_opti_cor = {"x": b3["x"]+(b2["x"]-b3["x"])/(7/4),"y": 0.5+(0.5-b3["y"])/(3/2)}
+    if roles["att"] == 1:
+        att = b1
+        if roles["goalie"] = 2:
+            goa = b2
+        elif roles["goalie"] = 3:
+            goa = b3
+    elif roles["att"] == 2:
+        att = b2
+        if roles["goalie"] = 1:
+            goa = b1
+        elif roles["goalie"] = 3:
+            goa = b3
+    elif roles["att"] == 3:
+        att = b3
+        if roles["goalie"] = 2:
+            goa = b2
+        elif roles["goalie"] = 1:
+            goa = b1
+        
+    supp_opti_cor = {"x": att["x"]+(goa["x"]-att["x"])/(7/4),"y": 0.5+(0.5-att["y"])/(3/2)}
     return supp_opti_cor
     
