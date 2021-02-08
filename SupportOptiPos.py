40a39da2138b903c2954c59128eaec04bbcddc36
Gekbako <jakub.gregus@gmail.com>
2021-02-08 01:19:56 +0100

    older version

--- SupportOptiPos.py
+++ SupportOptiPos.py
@@ -1,29 +1,22 @@
 from CoordinateRecalculator import robot_pos_recalc
 def support_position (data):
-    roles = role_decision(self)
-    b1 = {"x":data["B1"]["x"],"y": data["B1"]["y"]}
-    b2 = {"x":data["B2"]["x"],"y": data["B2"]["y"]}
-    b3 = {"x":data["B3"]["x"],"y": data["B3"]["y"]}
+    x_cor = {"b1":data["B1"]["x"],"b2":data["B2"]["x"],"b3":data["B3"]["x"]}
+    y_cor = {"b1": data["B1"]["y"], "b2": data["B2"]["y"], "b3": data["B3"]["y"]}
 
-    if roles["att"] == 1:
-        att = b1
-        if roles["goalie"] == 2:
-            goa = b2
-        elif roles["goalie"] == 3:
-            goa = b3
-    elif roles["att"] == 2:
-        att = b2
-        if roles["goalie"] == 1:
-            goa = b1
-        elif roles["goalie"] == 3:
-            goa = b3
-    elif roles["att"] == 3:
-        att = b3
-        if roles["goalie"] == 2:
-            goa = b2
-        elif roles["goalie"] == 1:
-            goa = b1
-        
-    supp_opti_cor = {"x": att["x"]+(goa["x"]-att["x"])/(7/4),"y": 0.5+(0.5-att["y"])/(3/2)}
+    # checking for attacker and goalie
+    if x_cor["b1"]==max(x_cor.values()):
+        goalie_cor = {"x":x_cor["b1"],"y":y_cor["b1"]}
+    if x_cor["b2"]==max(x_cor.values()):
+        goalie_cor = {"x":x_cor["b2"],"y":y_cor["b2"]}
+    if x_cor["b3"]==max(x_cor.values()):
+        goalie_cor = {"x":x_cor["b3"],"y":y_cor["b3"]}
+    if x_cor["b1"]==min(x_cor.values()):
+        att_cor = {"x":x_cor["b1"],"y":y_cor["b1"]}
+    if x_cor["b2"]==min(x_cor.values()):
+        att_cor = {"x":x_cor["b2"],"y":y_cor["b2"]}
+    if x_cor["b3"]==min(x_cor.values()):
+        att_cor = {"x":x_cor["b3"],"y":y_cor["b3"]}
+
+    #calculation of support optimal position
+    supp_opti_cor = {"x":att_cor["x"]+(goalie_cor["x"]-att_cor["x"])/2,"y":0.5+(0.5-att_cor["y"])/2}
     return supp_opti_cor
-    
